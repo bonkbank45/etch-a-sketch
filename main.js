cc13f970a46343bd6412c61745bb3e9d9fd43fa5
bonkbank45 <bankforever22@gmail.com>
2023-07-11 21:51:58 +0700

    Add feature changeSizeGrid

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const container = document.getElementById("container-row-col");
+const slider = document.getElementById("myRange");
+let outputSizeGrid = document.getElementById("grid-size-show");
 
 function createRow() {
     const row = document.createElement("div");
@@ -6,9 +8,20 @@ function createRow() {
     return row;
 }
 
+let holdState = false;
 function createCol() {
     const col = document.createElement("div");
     col.classList.add("col")
+    col.addEventListener("mousedown", (event) => {
+        holdState = true;
+        event.target.style.background = "black";
+    });
+    col.addEventListener("mouseup", () => {holdState = false;}); 
+    col.addEventListener("mouseover", (event) => {
+        if(holdState) {
+            event.target.style.background = "black";
+        }
+    });
     return col;
 }
 
@@ -23,36 +36,58 @@ function insertRowToContainer(row) {
     container.appendChild(row);
 }
 
-for (let i = 0; i < 16; i++) {
-    insertRowToContainer(insertColToRow(createRow(), 16));
+function changeGridSize() {
+
 }
+
+for (let i = 0; i < 32; i++) {
+    insertRowToContainer(insertColToRow(createRow(), 32));
+}
+
+function changeSizeGrid(amount) {
+    const difference = amount - container.childElementCount;
+    let previousRow = document.getElementsByClassName("row");
+    reset();
+    if (container.childElementCount < amount) {
+        for (row of previousRow) {
+            for (let i = 0; i < difference; i++) {
+            row.appendChild(createCol());
+            }
+        }
+        for (let i = 0; i < difference; i++) {
+            insertRowToContainer(insertColToRow(createRow(), amount));
+        }
+    }
+    if (container.childElementCount > amount){
+        for (row of previousRow) {
+            for (let i = 0; i > difference; i--) {
+            row.removeChild(row.firstChild);
+            }
+        }
+        for (let i = 0; i > difference; i--) {
+            container.removeChild(container.firstChild);
+        }
+    }
+}
+
+function reset() {
+    const cellPaints = document.querySelectorAll(".col[style]");
+    for (cell of cellPaints) {
+            cell.removeAttribute("style");
+    }
+}
+
+console.log(container.childElementCount);
 
 const cells = document.getElementsByClassName("col");
 const containerWrap = document.getElementsByClassName("container-wrap");
 containerWrap[0].addEventListener("mouseup", () => {holdState = false;})
 
-let holdState = false;
-for (cell of cells) {
-    cell.addEventListener("mousedown", (event) => {
-        holdState = true;
-        event.target.style.background = "black";
-    });
-    cell.addEventListener("mouseup", () => {holdState = false;}); 
-    cell.addEventListener("mouseover", (event) => {
-        if(holdState) {
-            event.target.style.background = "black";
-        }
-    });
-}
-
-// mousedown
-// mouseover = hover
-// (col) => col.target.style.background = "black"
-
 const resetButton = document.getElementById("reset");
-resetButton.addEventListener("click", () => {
-    const cellPaints = document.querySelectorAll(".col[style]");
-    for (cell of cellPaints) {
-            cell.removeAttribute("style");
-        }
-    });
+resetButton.addEventListener("click", reset);
+
+outputSizeGrid.textContent = `${slider.value} x ${slider.value}`;
+slider.addEventListener("input", (event) => {
+    outputSizeGrid.textContent = `${event.target.value} x ${event.target.value}`;
+    changeSizeGrid(event.target.value);
+});
