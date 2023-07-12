e20674df861a2880deea25c7aab43d20ab97de17
bonkbank45 <bankforever22@gmail.com>
2023-07-12 21:47:26 +0700

    Add some style css

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
-let statePen = true;
-let stateEraser = false;
-let stateRainbow = false;
-let stateDarken = false;
-let stateLighten = false;
-let holdState = false;
+let state = {
+    pen: true,
+    eraser: false,
+    rainbow: false,
+    darken: false,
+    lighten: false,
+    hold: false
+};
+
 let indexRainbow = 0;
+
 const RAINBOW_PALLETE = ["#33A8C7", "#52E3E1", "#A0E426", 
                          "#FDF148", "#FFAB00", "#F77976", 
                          "#F050AE", "#D883FF", "#9336FD"];
@@ -24,7 +28,7 @@ function createRow() {
 function createCol() {
     const col = document.createElement("div");
     col.classList.add("col")
-    setColor(col);
+    addEventToCol(col);
     col.style.background = "rgb(255,255,255)";
     return col;
 }
@@ -75,33 +79,29 @@ function removeGrid(previousGrid, difference) {
 }
 
 function stateBrush(event) {
-    if (statePen) {
+    if (state.pen) {
         event.target.style.background = colorPicker.value;}
-    else if (stateEraser) { event.target.style.background = "rgb(255,255,255)" }
-    else if (stateRainbow) {
+    else if (state.eraser) { event.target.style.background = "rgb(255,255,255)" }
+    else if (state.rainbow) {
         if (indexRainbow < 9) {
             event.target.style.background = RAINBOW_PALLETE[indexRainbow++];
         } else {
             indexRainbow = 0;
-            event.target.style.background = RAINBOW_PALLETE[indexRainbow++];
-        }
-    }
-    else if (stateDarken) {
-        event.target.style.background = setShade(returnSplitRgb(event.target.style.background), -10);
-    }
-    else if (stateLighten) {
-        event.target.style.background = setShade(returnSplitRgb(event.target.style.background),  10);
-    }
+            event.target.style.background = RAINBOW_PALLETE[indexRainbow++]; } }
+    else if (state.darken) {
+        event.target.style.background = setShade(returnSplitRgb(event.target.style.background), -10);}
+    else if (state.lighten) {
+        event.target.style.background = setShade(returnSplitRgb(event.target.style.background),  10);}
 }
 
-function setColor(col) {
+function addEventToCol(col) {
     col.addEventListener("mousedown", (event) => {
-        holdState = true;
+        state.hold = true;
         stateBrush(event);
     });
-    col.addEventListener("mouseup", () => {holdState = false;}); 
+    col.addEventListener("mouseup", () => {state.hold = false;}); 
     col.addEventListener("mouseover", (event) => {
-        if(holdState) {
+        if(state.hold) {
             stateBrush(event);
         }
     });
@@ -112,12 +112,10 @@ function returnSplitRgb(value) {
 }
 
 function setShade(rgbcolor,percent) { //#FF00AA
-
     // get value int hex color
     let R = rgbcolor[0];
     let G = rgbcolor[1];
     let B = rgbcolor[2];
-
     // calculate by percent
     if (R > 0 && G >= 0 && B >=  0) {
         R = R * ((100 + percent) / 100);
@@ -127,29 +125,10 @@ function setShade(rgbcolor,percent) { //#FF00AA
         R = G = B = 60;
     }
 
-    /*
-
-    // check value rgb is valid or not
-    R = (R > 255) ? R : "0";
-    G = (G > 255) ? G : "0";
-    B = (B > 255) ? B : "0";
-
-    // round number
-    R = Math.round(R);
-    G = Math.round(G);
-    B = Math.round(B);
-
-    // valid 1 digit number and return to string
-    let RR = (R.toString().length === 1) ? "0"+ R.toString(16) : R.toString(16);
-    let GG = (G.toString().length === 1) ? "0"+ G.toString(16) : G.toString(16);
-    let BB = (B.toString().length === 1) ? "0"+ B.toString(16) : B.toString(16);
-    */
-
-    // round number
     R = Math.floor(R);
     G = Math.floor(G);
     B = Math.floor(B);
-    
+
     return `rgb(${R},${G},${B})`
 }
 
@@ -157,7 +136,6 @@ function reset() {
     const gridPaints = document.querySelectorAll(".col[style]");
     for (gridPaint of gridPaints) {
         gridPaint.style.background = "rgb(255,255,255)";
-        //gridPaint.removeAttribute("style");
     }
 }
 
@@ -170,53 +148,53 @@ resetButton.addEventListener("click", reset);
 
 const eraserButton = document.getElementById("eraser");
 eraserButton.addEventListener("click", () => {
-    statePen = false;
-    stateRainbow = false;
-    stateDarken = false;
-    stateLighten = false;
-    stateEraser = true;
+    state.pen = false;
+    state.rainbow = false;
+    state.darken = false;
+    state.lighten = false;
+    state.eraser = true;
 });
 
 const penButton = document.getElementById("pen");
 penButton.addEventListener("click", () => {
-    stateRainbow = false;
-    stateEraser = false;
-    stateDarken = false;
-    stateLighten = false;
-    statePen = true;
+    state.rainbow = false;
+    state.eraser = false;
+    state.darken = false;
+    state.lighten = false;
+    state.pen = true;
 });
 
 const rainbowButton = document.getElementById("rainbow");
 rainbowButton.addEventListener("click", () => {
-    statePen = false;
-    stateEraser= false;
-    stateDarken = false;
-    stateLighten = false;
-    stateRainbow = true;
+    state.pen = false;
+    state.eraser = false;
+    state.darken = false;
+    state.lighten = false;
+    state.rainbow = true;
 });
 
 const darkenButton = document.getElementById("darken");
 darkenButton.addEventListener("click", () => {
-    statePen = false;
-    stateEraser= false;
-    stateRainbow = false;
-    stateLighten = false;
-    stateDarken = true;
+    state.pen = false;
+    state.eraser = false;
+    state.rainbow = false;
+    state.lighten = false;
+    state.darken = true;
 });
 
 const lightenButton = document.getElementById("lighten");
 lightenButton.addEventListener("click", () => {
-    statePen = false;
-    stateEraser= false;
-    stateRainbow = false;
-    stateDarken = false;
-    stateLighten = true;
-})
+    state.pen = false;
+    state.eraser = false;
+    state.rainbow = false;
+    state.darken = false;
+    state.lighten = true;
+});
 
 colorPicker.addEventListener("change", (event) => {
     const cols = document.getElementsByClassName("col");
     for (col of cols) {
-        setColor(col);
+        addEventToCol(col);
     }
 }, false);
 
